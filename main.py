bcc0079a1210774bc1912aef521f0188d62ca549
Oshwald Khoo <oshwald.khoo@gmail.com>
2023-05-04 09:26:07 +1000

    created main file

---
+++ main.py
@@ -0,0 +1,21 @@
+from roster_generator import gen_dates_times, format_result
+
+t = { "Monday" : ["06:00", "14:30"],
+      "Tuesday" : ["06:00", "14:30"],
+      "Wednesday" : ["19:00", "00:00"],
+      "Thursday" : ["", ""],
+      "Friday" : ["", ""],
+      "Saturday" : ["06:00", "16:00"],
+      "Sunday" : ["", ""],
+}
+
+t = { "Monday" : ["19:00", "00:00"],
+      "Tuesday" : ["19:00", "00:00"],
+      "Wednesday" : ["19:00", "00:00"],
+      "Thursday" : ["19:00", "00:00"],
+      "Friday" : ["19:00", "00:00"],
+      "Saturday" : ["19:00", "00:00"],
+      "Sunday" : ["19:00", "00:00"],
+}
+
+print(format_result(gen_dates_times(1, t)))
--- roster_generator.py
+++ roster_generator.py
@@ -172,24 +172,3 @@ def format_result(result):
 
     f_result = f_result.rstrip("\n")
     return f_result
-
-
-t = { "Monday" : ["06:00", "14:30"],
-      "Tuesday" : ["06:00", "14:30"],
-      "Wednesday" : ["19:00", "00:00"],
-      "Thursday" : ["", ""],
-      "Friday" : ["", ""],
-      "Saturday" : ["06:00", "16:00"],
-      "Sunday" : ["", ""],
-}
-
-t = { "Monday" : ["19:00", "00:00"],
-      "Tuesday" : ["19:00", "00:00"],
-      "Wednesday" : ["19:00", "00:00"],
-      "Thursday" : ["19:00", "00:00"],
-      "Friday" : ["19:00", "00:00"],
-      "Saturday" : ["19:00", "00:00"],
-      "Sunday" : ["19:00", "00:00"],
-}
-
-print(format_result(gen_dates_times(1, t)))
